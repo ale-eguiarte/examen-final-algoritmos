ccd0644cc6ec597ee3c02279c09ac01872ba52e4
0282293 <0282293@up.edu.mx>
2025-11-27 19:05:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,4 +2,20 @@
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+#include <string>
 using namespace std;
+
+int main() {
+  string clave4 = "secretoslos";
+  int rot = 4;
+  
+  rot = rot % clave4.length();
+  
+  string parteFinal = clave4.substr(clave4.length() - rot);
+  string parteInicial = clave4.substr(0, clave4.length() - rot);
+  
+  string respuesta = parteFinal + parteInicial;
+  
+  cout << "Clave descifrada: " << respuesta << endl; //no tiene espacio 
+  
+}
