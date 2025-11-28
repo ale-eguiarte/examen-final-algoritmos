f19dec0d83f4b6ff68db84242666d242ae621542
0281572-afk <0281572@up.edu.mx>
2025-11-27 20:15:45 -0600

    Final main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main() {
   string clave4 = "secretoslos ";
-  int rot = 4;
+  int rot = 4; // número de lugares que se rota
   
-  rot = rot % clave4.length();
+  rot = rot % clave4.length(); // si la rotación es mayor a la clave, evita que de vueltas y solo mueve los lugares necesarios
   
-  string parteFinal = clave4.substr(clave4.length() - rot);
-  string parteInicial = clave4.substr(0, clave4.length() - rot); 
-  string descifrado = parteFinal + parteInicial;
+  string parteFinal = clave4.substr(clave4.length() - rot); // “los” - la parte que rota
+  string parteInicial = clave4.substr(0, clave4.length() - rot);  // “secretos” - la parte restante
+  string descifrado = parteFinal + parteInicial; //junta ambas partes
   
   cout << descifrado<< endl;
 
