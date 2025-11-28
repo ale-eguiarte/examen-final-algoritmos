2d463e3d761c5c78f16304d62a85e76ce3c4d92e
ale-eguiarte <0281587@up.edu.mx>
2025-11-27 20:10:33 -0600

    espacio al final de la clave, evita tener q moverlo despues

--- main.cpp
+++ main.cpp
@@ -1,24 +1,17 @@
-#include <math.h>
 #include <iostream>
-#include <algorithm>
-#include <iomanip>
 #include <string>
 using namespace std;
 
 int main() {
-  string clave4 = "secretoslos";
+  string clave4 = "secretoslos ";
   int rot = 4;
   
   rot = rot % clave4.length();
   
-  string parteFinal = clave4.substr(clave4.length() - rot); //te da "slos"
-  string parteInicial = clave4.substr(0, clave4.length() - rot); //te da "secreto"
-  string descifrado = parteFinal + parteInicial; //"slossecreto"
+  string parteFinal = clave4.substr(clave4.length() - rot);
+  string parteInicial = clave4.substr(0, clave4.length() - rot); 
+  string descifrado = parteFinal + parteInicial;
   
-  //las acomoda con espacio
-  string parte1 = parteFinal.substr(1);          //”los”, el substr quita el s antes de los 
-  string parte2 = parteInicial + parteFinal[0]; //”secreto” agrega la s que faltaba, parteFinal[0] es ‘s’, s esta en posición 0 de descifrado
-  
-  cout << parte1 << " " << parte2 << endl;
+  cout << descifrado<< endl;
 
 }
