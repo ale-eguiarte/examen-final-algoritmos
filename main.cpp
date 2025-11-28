c12b59f340e02af5a9110b79879cbf78a0348ef6
0281572-afk <0281572@up.edu.mx>
2025-11-27 19:45:53 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,16 +11,14 @@ int main() {
   
   rot = rot % clave4.length();
   
-  string parteFinal = clave4.substr(clave4.length() - rot);
-  string parteInicial = clave4.substr(0, clave4.length() - rot);
+  string parteFinal = clave4.substr(clave4.length() - rot); //te da "slos"
+  string parteInicial = clave4.substr(0, clave4.length() - rot); //te da "secreto"
+  string descifrado = parteFinal + parteInicial; //"slossecreto"
   
-  string respuesta = parteFinal + parteInicial;
-
   //las acomoda con espacio
-  string parte1 = descifrado.substr(1, 3);
-  string parte2 = descifrado.substr(4);
-
-  cout << parte1 << " " << parte2 << endl; //y ahora no tiene la s
-
+  string parte1 = parteFinal.substr(1);          //”los”, el substr quita el s antes de los 
+  string parte2 = parteInicial + parteFinal[0]; //”secreto” agrega la s que faltaba, parteFinal[0] es ‘s’, s esta en posición 0 de descifrado
   
+  cout << parte1 << " " << parte2 << endl;
+
 }
